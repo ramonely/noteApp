c1d2f2e7cbf908bfe0fa57309727cb961bf07bff
Erin Walter <erinwalter@rocketmortgage.com>
2021-11-04 10:20:58 -0400

    Made Branch more modular and got all functionality out of main so that I could Start Unit testing

---
+++ noteApp/Journal.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace noteApp
+{
+    class Journal
+    {
+        // in order to do unit testing, I needed to move everything out of main,
+        //so i made a new "journal" class to house the functionality of this program
+        //elsewhere outside of main. 
+        public List<Note> Tasks = new List<Note>();
+
+        public Journal()
+        {
+            //added some dummy notes to the constructor class only for testing purposes, will comment out later
+            Tasks.Add(new Note("Clean Office", "Clean surfaces, sweep, vacuum, wash floors.", DateTime.Now));
+            Tasks.Add(new Note("Finish Lab", "Lab is Due Soon! Rework main method.", DateTime.Now));
+            Tasks.Add(new Note("Grocery List", "Make sure you don't forget Eggs", DateTime.Now));
+        }
+
+        //also taking functionality of each menu option out of main menu and adding here 
+        //and renaming them as separate methods
+
+        public void AddTask()
+        {
+            //previously housed in main method, put here instead
+            Console.Write("\nWhat would you like to call your Note?: ");
+            string noteName = Console.ReadLine();
+            Console.Write($"What are you adding into your \"{noteName}\" note?: ");
+            string noteMessage = Console.ReadLine();
+            Tasks.Add(new Note(noteName, noteMessage, DateTime.Now));
+            Console.WriteLine("Update Complete\n");
+        }
+
+        //took functionality out of main menu if statment and moved here
+        public void EditTask()
+        {
+            //called to display all notes so could be reused.
+            DisplayAllTasks();
+
+            //moved input validation to its own method so
+            //it could be shared with Delete and Edit Tasks methods
+            Note editNote = GetValidInput("Choose note to edit: ");
+
+            //put edit functionality at the object level in a new method
+            //inside the note class
+            editNote.EditNote();
+
+        }
+
+        //took this functionality out of main and added here
+        public void DeleteTask()
+        {
+            //instead of printing out a display manually, just caling to display all again
+            DisplayAllTasks();
+
+            //took out validation from this method and now am calling to another
+            //for input validation
+            Note delNote = GetValidInput("Select which task to delete: ");
+
+            //searches through journal to find that journal entry then deletes
+            foreach(Note note in Tasks)
+            {
+                if(note == delNote)
+                {
+                    Tasks.Remove(note);
+                    Console.WriteLine("Task deleted successfully");
+                    break;
+                }
+            }
+           
+        }
+
+        //took this functionality out of main and added here 
+        public void DisplayAllTasks()
+        {
+            //method to view all tasks in Journal List
+            //took code from main method and added here
+            Console.WriteLine($"\nID\tTITLE\t\tDATE ADDED\tMESSAGE");
+            Console.WriteLine("---------------------------------------------------------------");
+
+            for (int i = 0; i < Tasks.Count; i++)
+            {
+                Console.WriteLine($"{i}: {Tasks[i]}");
+            }
+
+            if (Tasks.Count == 0)
+            {
+                Console.WriteLine("\nThere is nothing in your Note App.\n");
+            }
+        }
+
+        //there used to be two places where input was validated
+        //both served the same purpose though and code was almost identical
+        //so just moved it to its own method so it could be called to
+        public Note GetValidInput(string prompt)
+        {
+            int choice = -1;
+            Note noteChoice;
+            while (true)
+            {
+                try
+                {
+                    Console.Write(prompt);
+                    choice = int.Parse(Console.ReadLine());
+                    noteChoice = Tasks[choice];
+                    break;
+                }
+                catch (FormatException)
+                {
+                    {
+                        Console.Write("\nInvalid Selection, try again.");
+                        continue;
+                    }
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    Console.Write($"\nPlease only enter number between 0 and {Tasks.Count-1}. Try Again. ");
+                    continue;
+                }
+            }
+            return noteChoice;
+        }
+    }
+}
--- noteApp/Note.cs
+++ noteApp/Note.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace noteApp
 {
-    internal class Note
+    class Note
     {
         public string Title { get; set; }
         public string Message { get; set; }
@@ -16,6 +14,45 @@ namespace noteApp
             Title = title;
             Message = message;
             Date = date;
+        }
+
+        //took this out of main and added to the note object as it is editing properties at this level
+        //so to keep functionality modular adding it here
+        public void EditNote()
+        {
+            Console.Write($"You selected:\n{ToString()}\nDo you want to edit (1)Title or (2)Message: ");
+            string edit = Console.ReadLine();
+            while (true)
+            {
+                if (edit == "1")
+                {
+                    Console.Write($"Enter new Title: ");
+                    string newN = Console.ReadLine();
+                    Title = newN;
+                    break;
+                }
+                else if (edit == "2")
+                {
+
+                    Console.Write("Enter new Message: ");
+                    string newM = Console.ReadLine();
+                    Message = newM;
+                    break;
+                }
+                else
+                {
+
+                    Console.Write("Please only pick (1) or (2): ");
+                    continue;
+                }
+            }
+            Console.WriteLine("Update Complete");
+        }
+
+        //made a tostring override so it would be simpler to print out a note in uniform format
+        public override string ToString()
+        {
+            return $"\t{Title}\t{Date.ToString("MM/dd/yyyy")}\t{Message}";
         }
     }
 }
